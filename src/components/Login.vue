36348ffe99ddba282f6f4c64526c6807f842662f
NestorJavier <nestorjavier@alumnos.uaslp.edu.mx>
2020-03-09 13:23:10 -0600

    Login sin terminar

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6 lg5 xl4>
+               <v-card>
+                   <v-toolbar dark color="blue darken-3">
+                    <v-toolbar-title>
+                        Acceso al sistema
+                    </v-toolbar-title>
+                   </v-toolbar>
+                   <v-card-text>
+                       
+                   </v-card-text>
+                   <v-card-actions class="px-3 pb-3">
+                       
+                   </v-card-actions>
+               </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+<script>
+export default {
+    
+}
+</script>
